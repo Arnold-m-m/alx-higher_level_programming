0ce8e3c3822253d5b37d0503cc37279bdc89405f
Arnold-m-m <mwangiarnold0@gmail.com>
2024-01-23 12:43:58 +0000

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'e' and chr(letter) != 'q':
-        print("{}".format(chr(letter)), end="")
+for x in range(97, 123):
+    print("{}".format(chr(x)), end="")
